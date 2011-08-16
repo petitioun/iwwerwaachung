8f001b90b95441c8905aa8fe80fa75cff08f411a
Sven Clement <git@sven.lu>
2011-08-16 20:14:07 +0200

    blank rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwwerwaachung::Application.config.secret_token = '75ab6424b8f532da6d611d523b0c513673d6a92af602a85c8b1b47aa910e40dcc719532caff2264394aeb34b71cf0621a47e5d61c00e3a74524c87f50d9cdf04'
